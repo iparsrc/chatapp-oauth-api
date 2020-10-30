22d1888e34fa311319eeae86eb4b4fc7db9c24a9
parsa <akbariparsa1209@gmail.com>
2020-10-30 23:27:19 +0330

    Moved parsing out.

--- domain/verify.go
+++ domain/verify.go
@@ -3,42 +3,7 @@ package domain
 import (
 	"errors"
 	"time"
-
-	"github.com/dgrijalva/jwt-go"
 )
-
-// keyFunc returns a secret key based on the signing method.
-func keyFunc(token *jwt.Token) (interface{}, error) {
-	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-		return nil, errors.New("error: unexpected signing method")
-	}
-	return []byte(getSecretKey()), nil
-}
-
-// parseToken parses the token and returns the claims.
-func parseToken(tokenStr string) (int64, string, string, error) {
-	token, err := jwt.Parse(tokenStr, keyFunc)
-	if err != nil {
-		return 0, "", "", err
-	}
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if !ok {
-		return 0, "", "", errors.New("error: can't parse claims")
-	}
-	exp, ok := claims["exp"].(int64)
-	if !ok {
-		return 0, "", "", errors.New("error: can't parse exp")
-	}
-	userID, ok := claims["user_id"].(string)
-	if !ok {
-		return 0, "", "", errors.New("error: can't parse uesr_id")
-	}
-	uuid, ok := claims["uuid"].(string)
-	if !ok {
-		return 0, "", "", errors.New("error: can't parse uuid")
-	}
-	return exp, userID, uuid, nil
-}
 
 // checkExipirationDate validates that the token is not expired.
 func checkExpirationDate(exp int64) error {
