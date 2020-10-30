d2af2e4045ece11aa690607ee8278c5500f53641
parsa <akbariparsa1209@gmail.com>
2020-10-30 23:04:19 +0330

    Created refresh.go & Refresh func.

---
+++ domain/refresh.go
@@ -0,0 +1,32 @@
+package domain
+
+// deletePreviousRefreshToken removes the token from redis.
+func deletePreviousRefreshToken(uuid string) error {
+	_, err := client.Del(uuid).Result()
+	return err
+}
+
+// Refresh function takes a refresh token and creates new tokens.
+// It will return empty strings as tokens and an error if the
+// refresh token is invalid or something goes wrong.
+func Refresh(refreshToken string) (at, rt string, e error) {
+	// 1. Verify the refresh token.
+	userID, uuid, err := Verify(refreshToken)
+	if err != nil {
+		return "", "", err
+	}
+
+	// 2. Delete the previous refresh token from the redis.
+	if err := deletePreviousRefreshToken(uuid); err != nil {
+		return "", "", err
+	}
+
+	// 3. Create new tokens.
+	at, rt, err = Create(userID)
+	if err != nil {
+		return "", "", err
+	}
+
+	// 4. Return values.
+	return at, rt, nil
+}
