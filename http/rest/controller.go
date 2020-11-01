b05159c6320cf1a1e66a09860e97f14f6bf6b499
parsa <akbariparsa1209@gmail.com>
2020-11-01 16:33:23 +0330

    Improved error handling.

--- http/rest/controller.go
+++ http/rest/controller.go
@@ -18,11 +18,7 @@ func create(c *gin.Context) {
 	// 1. Get user_id from the request url.
 	userID := c.Query("user_id")
 	if userID == "" {
-		restErr := &utils.RestErr{
-			Message: "user_id is not specified.",
-			Status:  400,
-			Error:   "error: bad request",
-		}
+		restErr := utils.BadRequest("user_id is not specified.")
 		c.JSON(restErr.Status, restErr)
 		return
 	}
@@ -30,11 +26,7 @@ func create(c *gin.Context) {
 	// 2. Create access and refresh tokens.
 	accessToken, refreshToken, err := s.Create(userID)
 	if err != nil {
-		restErr := &utils.RestErr{
-			Message: "can't operate creation.",
-			Status:  500,
-			Error:   err.Error(),
-		}
+		restErr := utils.InternalServerErr("can't operate creation.")
 		c.JSON(restErr.Status, restErr)
 		return
 	}
@@ -50,11 +42,7 @@ func refresh(c *gin.Context) {
 	// 1. Get refresh token from the cookies.
 	refreshToken, err := c.Cookie("refresh_token")
 	if err != nil {
-		restErr := &utils.RestErr{
-			Message: "refresh_token cookie is not set.",
-			Status:  400,
-			Error:   "error: bad request",
-		}
+		restErr := utils.BadRequest("refresh_token cookie is not set.")
 		c.JSON(restErr.Status, restErr)
 		return
 	}
@@ -62,11 +50,7 @@ func refresh(c *gin.Context) {
 	// 2. Create new access and refresh tokens.
 	newAccessToken, newRefreshToken, err := s.Refresh(refreshToken)
 	if err != nil {
-		restErr := &utils.RestErr{
-			Message: "can't operate refreshing.",
-			Status:  400,
-			Error:   err.Error(),
-		}
+		restErr := utils.BadRequest("can't operate refreshing.")
 		c.JSON(restErr.Status, restErr)
 		return
 	}
@@ -84,11 +68,7 @@ func verify(c *gin.Context) {
 	// 1. Get access token from cookies.
 	accessToken, err := c.Cookie("access_token")
 	if err != nil {
-		restErr := &utils.RestErr{
-			Message: "access_token cookie is not set.",
-			Status:  400,
-			Error:   "error: bad request",
-		}
+		restErr := utils.BadRequest("access_token cookie is not set.")
 		c.JSON(restErr.Status, restErr)
 		return
 	}
@@ -96,11 +76,7 @@ func verify(c *gin.Context) {
 	// 2. Verify the access token.
 	_, _, err = s.Verify(accessToken)
 	if err == domain.ErrParseToken {
-		restErr := &utils.RestErr{
-			Message: "can't operate verification.",
-			Status:  400,
-			Error:   err.Error(),
-		}
+		restErr := utils.BadRequest("can't operate verification.")
 		c.JSON(restErr.Status, restErr)
 		return
 	}
@@ -118,11 +94,7 @@ func revoke(c *gin.Context) {
 	// 1. Get the refresh token from cookies.
 	refreshToken, err := c.Cookie("refresh_token")
 	if err != nil {
-		restErr := &utils.RestErr{
-			Message: "refresh_token cookie is not set.",
-			Status:  400,
-			Error:   "error: bad request",
-		}
+		restErr := utils.BadRequest("refresh_token cookie is not set.")
 		c.JSON(restErr.Status, restErr)
 		return
 	}
@@ -130,25 +102,17 @@ func revoke(c *gin.Context) {
 	// 2. Get the access token from cookies.
 	accessToken, err := c.Cookie("access_token")
 	if err != nil {
-		restErr := &utils.RestErr{
-			Message: "access_token cookie is not set.",
-			Status:  400,
-			Error:   "error: bad request",
-		}
+		restErr := utils.BadRequest("access_token cookie is not set.")
 		c.JSON(restErr.Status, restErr)
 		return
 	}
 
 	// 3. Revoke the tokens.
 	if err := s.Revoke(accessToken, refreshToken); err != nil {
-		restErr := &utils.RestErr{
-			Message: "can't operate revokation.",
-			Status:  400,
-			Error:   err.Error(),
-		}
+		restErr := utils.BadRequest("can't operate revokation.")
 		c.JSON(restErr.Status, restErr)
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": "tokens are revoked"})
+	c.JSON(http.StatusOK, gin.H{"succeed": true})
 	return
 }
